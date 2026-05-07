dd5b6576ccff4813a09551777f12e33a54e264ad
Henri Heyden <henri@heyden5.de>
2026-05-07 16:37:15 +0200

    Cleaned up the code for the peripheral and temperature library.

--- Lab1/code/pt1/brodcaster/src/temperature.c
+++ Lab1/code/pt1/brodcaster/src/temperature.c
@@ -2,9 +2,12 @@
 
 void set_temperature_random(double *temperature)
 {
+    // Generate a random unsigned byte.
     uint8_t base;
     sys_rand_get(&base, sizeof(base));
 
+    // Random temperature between -10.0 and 30.0 degrees Celsius.
+    // (base / 255.0) gives a value between 0.0 and 1.0, which we then scale to the desired range.
     *temperature = (base / 255.0) * 40.0 - 10.0;
 }
 
@@ -15,14 +18,14 @@ void set_temperature_constant(double *temperature)
 
 void set_temperature_random_walk(double *temperature)
 {
+    // ...
     uint8_t base;
     sys_rand_get(&base, sizeof(base));
     
     // Random change of 1.0 degrees at most.
-    *temperature += 2.0 * (base / 255) - 1.0;
+    *temperature += 2.0 * (base / 255.0) - 1.0;
 }
 
-// Calls method based on METHOD parameter.
 void update_temperature(double *temperature)
 {
 #if METHOD == 0
--- Lab1/code/pt2/peripheral/src/main.c
+++ Lab1/code/pt2/peripheral/src/main.c
@@ -1,54 +1,12 @@
-#include <zephyr.h>
+#include "main.h"
 
-#include <zephyr/types.h>
-#include <stddef.h>
-#include <sys/printk.h>
-#include <sys/util.h>
-
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/gatt.h>
-
-#include "temperature.h"
-
-#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
-
-static double current_temperature = 20.0;
-
-// FOR UUIDS, SEE REF A: https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Assigned_Numbers/out/en/Assigned_Numbers.pdf
-
-static struct bt_uuid_16 service_uuid = BT_UUID_INIT_16(0x1809); // UUID for health thermometor service (A section 3.4.1 Services by Name)
-static struct bt_uuid_16 char_uuid = BT_UUID_INIT_16(0x2A1C);    // Temperature measurement (A section 3.8.1 Characteristics by Name)
-
-/* Set Advertisement data */
-// Little-endian encoding
-static struct bt_data ad[] = {
-    // General discoverable, BR/EDR not supported
-    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR),
-
-    // Generic Thermometer (A section 2.6.3 Apperance Sub-category values)
-    BT_DATA_BYTES(BT_DATA_GAP_APPEARANCE, 0x00, 0x03),
-
-    // Service UUID
-    BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0x09, 0x18),
-};
-
-// Read callback for the temperature characteristic
 static ssize_t read_temperature_callback(struct bt_conn *conn, const struct bt_gatt_attr *attr,
                                          void *buf, uint16_t len, uint16_t offset)
 {
+    // Execute the read callback and store the result in the provided buffer.
+    // (the temperature is stored in the user_data field of the attribute.)
     return bt_gatt_attr_read(conn, attr, buf, len, offset, (double *)attr->user_data, sizeof(double));
 }
-
-// GATT service declaration and registration.
-BT_GATT_SERVICE_DEFINE(temperature_service,
-                       BT_GATT_PRIMARY_SERVICE(&service_uuid),
-                       BT_GATT_CHARACTERISTIC(&char_uuid.uuid, BT_GATT_CHRC_READ, BT_GATT_PERM_READ, read_temperature_callback, NULL, &current_temperature),
-                       // BT_GATT_CCC(callback, BT_GATT_PERM_READ), // Implement CCC change callback if we want to support notifications (asynchronous updates)
-);
-
-// No scan response data needed, but could be added.
 
 static void bt_ready(int err)
 {
@@ -56,6 +14,7 @@ static void bt_ready(int err)
     bt_addr_le_t addr = {0};
     size_t count = 1;
 
+    // If bt_enable failed, print an error message and return.
     if (err)
     {
         printk("Bluetooth init failed (err %d)\n", err);
@@ -64,13 +23,13 @@ static void bt_ready(int err)
 
     printk("Bluetooth initialized\n");
 
-    /* Start advertising */
+    // Start advertising the device with the specified advertising data and parameters.
     err = bt_le_adv_start(
         BT_LE_ADV_PARAM(
-            BT_LE_ADV_OPT_CONNECTABLE, // Allow connections
-            BT_GAP_ADV_FAST_INT_MIN_2,
-            BT_GAP_ADV_FAST_INT_MAX_2,
-            NULL),
+            BT_LE_ADV_OPT_CONNECTABLE, // Allow connections.
+            BT_GAP_ADV_FAST_INT_MIN_2, // 100ms minimum advertising interval.
+            BT_GAP_ADV_FAST_INT_MAX_2, // 150ms maximum advertising interval.
+            NULL), // Undirected advertising.
         ad,
         ARRAY_SIZE(ad),
         NULL, // No scan response data.
@@ -81,19 +40,14 @@ static void bt_ready(int err)
         return;
     }
 
-    /* For connectable advertising you would use
-     * bt_le_oob_get_local().  For non-connectable non-identity
-     * advertising an non-resolvable private address is used;
-     * there is no API to retrieve that.
-     */
-
+    // Get the Bluetooth address of the device and print it.
     bt_id_get(&addr, &count);
     bt_addr_le_to_str(&addr, addr_s, sizeof(addr_s));
 
+    printk("Brodcaster started, advertising as %s\n", addr_s);
+
     // The GATT service does not have to be explicitly registered, as this is already done by the BT_GATT_SERVICE_DEFINE macro.
     // However, if we had to register it manually, we would call bt_gatt_service_register() here.
-
-    printk("Brodcaster started, advertising as %s\n", addr_s);
 }
 
 int main(void)
@@ -102,14 +56,15 @@ int main(void)
 
     printk("Starting Temperature Brodcaster\n");
 
-    /* Initialize the Bluetooth Subsystem */
+    // Initialize the Bluetooth subsystem, continue with bt_ready callback when finished.
     err = bt_enable(bt_ready);
     if (err)
     {
         printk("Bluetooth init failed (err %d)\n", err);
     }
 
-    printk("Updating temperature data every 5 seconds\n");
+    // After starting advertising, we can update the temperature value periodically.
+    printk("Updating temperature data every 5 seconds...\n");
     while (1)
     {
         k_sleep(K_SECONDS(5));
